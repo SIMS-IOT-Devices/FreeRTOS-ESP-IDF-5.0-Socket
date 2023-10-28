1744b0af76cb28a1960b1ee7070d6c6c052165c6
SIMS IOT Devices <92898043+SIMS-IOT-Devices@users.noreply.github.com>
2023-10-28 05:56:26 +0300

    Add files via upload

---
+++ proj3.c
@@ -0,0 +1,134 @@
+// TCP SOCKET Client with WiFi connection communication via Socket
+
+#include <stdio.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+#include "freertos/event_groups.h"
+#include "freertos/queue.h"
+
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_check.h"
+#include "esp_mac.h"
+#include "esp_eth.h"
+#include "esp_netif.h"
+#include "esp_http_server.h"
+#include "esp_http_client.h"
+#include "esp_event.h"
+#include "esp_system.h"
+
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
+#include "lwip/sockets.h"
+#include "lwip/ip_addr.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "nvs_flash.h"
+#include "ping/ping_sock.h"
+#include "driver/gpio.h"
+#include "my_data.h"
+
+#define PORT 3333
+static const char *TAG = "TCP SOCKET Client";
+static const char *payload = "Message from ESP32 TCP Socket Client";
+
+void tcp_client(void)
+{
+    char rx_buffer[128];
+    char host_ip[] = "192.168.1.160"; // Server IP
+    int addr_family = 0;
+    int ip_protocol = 0;
+
+    while (1)
+    {
+        struct sockaddr_in dest_addr;
+        inet_pton(AF_INET, host_ip, &dest_addr.sin_addr);
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+
+        int sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+        if (sock < 0)
+        {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created ");
+        ESP_LOGI(TAG, "Connecting to %s:%d", host_ip, PORT);
+
+        int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        if (err != 0)
+        {
+            ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Successfully connected");
+
+        send(sock, payload, strlen(payload), 0);
+
+        int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+        rx_buffer[len] = 0; // Null-terminate
+        ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+        ESP_LOGI(TAG, "%s", rx_buffer);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+        if (sock != -1)
+        {
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+}
+
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    switch (event_id)
+    {
+    case WIFI_EVENT_STA_START:
+        printf("WiFi connecting WIFI_EVENT_STA_START ... \n");
+        break;
+    case WIFI_EVENT_STA_CONNECTED:
+        printf("WiFi connected WIFI_EVENT_STA_CONNECTED ... \n");
+        break;
+    case WIFI_EVENT_STA_DISCONNECTED:
+        printf("WiFi lost connection WIFI_EVENT_STA_DISCONNECTED ... \n");
+        break;
+    case IP_EVENT_STA_GOT_IP:
+        printf("WiFi got IP ... \n\n");
+        break;
+    default:
+        break;
+    }
+}
+
+void wifi_connection()
+{
+    nvs_flash_init();
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&wifi_initiation);
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+    wifi_config_t wifi_configuration = {
+        .sta = {
+            .ssid = SSID,
+            .password = PASS}};
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_start();
+    esp_wifi_connect();
+}
+
+void app_main(void)
+{
+    wifi_connection();
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    tcp_client();
+}
